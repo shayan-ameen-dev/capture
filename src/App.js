058bdf8830acf7eada35d2da10d602f912746611
shayan <shayan.ameen.dev@gmail.com>
2022-09-17 17:31:22 +0500

    creating nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import GlobalStyle from './components/GlobalStyle';
 
+import Nav from './components/Nav';
 import AboutUs from './pages/AboutUs';
 
 const App = () => {
   return (
     <div className='App'>
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <a id='logo' href='#'>
+          Capture
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href='#'>1. About Us</a>
+        </li>
+        <li>
+          <a href='#'>2. Our Work</a>
+        </li>
+        <li>
+          <a href='#'>1. Contact Us</a>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background: #282828;
+
+  #logo {
+    font-size: 1.5rem;
+    font-family: 'Lobster', cursive;
+    font-weight: lighter;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+  }
+
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+export default Nav;
