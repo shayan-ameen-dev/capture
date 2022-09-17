8e113a3550e017fe3067e9c0e096e846e4c58d1f
shayan <shayan.ameen.dev@gmail.com>
2022-09-17 17:57:54 +0500

    adding routing for work and contact pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+
 import GlobalStyle from './components/GlobalStyle';
 
 import Nav from './components/Nav';
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 
 const App = () => {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path='/' exact element={<AboutUs />} />
+        <Route path='/work' element={<OurWork />} />
+        <Route path='/contact' element={<ContactUs />} />
+      </Routes>
     </div>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,23 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a id='logo' href='#'>
+        <Link id='logo' to='/'>
           Capture
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href='#'>1. About Us</a>
+          <Link to='/'>1. About Us</Link>
         </li>
         <li>
-          <a href='#'>2. Our Work</a>
+          <Link to='/work'>2. Our Work</Link>
         </li>
         <li>
-          <a href='#'>1. Contact Us</a>
+          <Link to='/contact'>1. Contact Us</Link>
         </li>
       </ul>
     </StyledNav>
